a0811185d690ab6432d951d60c3129e31c8a8d42
Олександр <102046268+AlexElektrik@users.noreply.github.com>
2022-12-01 18:46:19 +0200

    Помилка деплоя

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import data from '../data';
 import user from '../user';
 import friends from '../friends';
 import transaction from '../transactions';
-import { GlobalStyle} from "./GlobalStyles";
+import { GlobalStyle, Container} from "./GlobalStyles";
 
 
 
